eebe8763081b0798077e4a2a45929eba44778ad6
littlelazymeerkat <esperanza.rugullis@gmail.com>
2025-12-09 19:22:33 +0100

    V3.2

--- src/test/java/bs/lf10/service/BookServiceTest.java
+++ src/test/java/bs/lf10/service/BookServiceTest.java
@@ -1,13 +1,9 @@
-
-Du:
 package bs.lf10.service;
 
 import bs.lf10.entity.Book;
 import bs.lf10.repository.BookRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
 import org.springframework.mock.web.MockMultipartFile;
 
 import java.io.IOException;
@@ -30,7 +26,6 @@ class BookServiceTest {
 
     @Test
     void saveBook_shouldConvertImagesToBase64_andSave() throws IOException {
-        // Arrange
         Book book = new Book();
 
         MockMultipartFile cover = new MockMultipartFile(
@@ -48,13 +43,7 @@ class BookServiceTest {
 
         when(bookRepository.save(any(Book.class))).thenAnswer(i -> i.getArgument(0));
 
-        // Act
         Book savedBook = bookService.saveBook(book, cover, spine, List.of(additional1, additional2));
-
-        // Assert Base64 encoding
-        assertNotNull(savedBook.getCoverImage());
-        assertNotNull(savedBook.getSpineImage());
-        assertEquals(2, savedBook.getAdditionalImages().size());
 
         assertEquals(
                 java.util.Base64.getEncoder().encodeToString("coverData".getBytes()),
@@ -66,59 +55,8 @@ class BookServiceTest {
                 savedBook.getSpineImage()
         );
 
+        List<String> additionalImages = savedBook.getAdditionalImages();
+        assertNotNull(additionalImages);
+        assertEquals(2, additionalImages.size());
         assertEquals(
-                java.util.Base64.getEncoder().encodeToString("addData1".getBytes()),
-                savedBook.getAdditionalImages().get(0)
-        );
-
-        // Verify save() was called
-        verify(bookRepository, times(1)).save(any(Book.class));
-    }
-
-    @Test
-    void saveBook_shouldNotFailWhenImagesAreNull() throws IOException {
-        Book book = new Book();
-
-        when(bookRepository.save(any(Book.class))).thenAnswer(i -> i.getArgument(0));
-
-        Book savedBook = bookService.saveBook(book, null, null, null);
-
-        assertNull(savedBook.getCoverImage());
-        assertNull(savedBook.getSpineImage());
-        assertNull(savedBook.getAdditionalImages());
-    }
-
-    @Test
-    void getAllBooks_shouldReturnList() {
-        List<Book> books = List.of(new Book(), new Book());
-
-        when(bookRepository.findAll()).thenReturn(books);
-
-        List<Book> result = bookService.getAllBooks();
-
-        assertEquals(2, result.size());
-        verify(bookRepository).findAll();
-    }
-
-    @Test
-    void getBookById_shouldReturnBook() {
-        Book book = new Book();
-        book.setId(1L);
-
-        when(bookRepository.findById(1L)).thenReturn(Optional.of(book));
-
-        Optional<Book> result = bookService.getBookById(1L);
-
-        assertTrue(result.isPresent());
-        assertEquals(1L, result.get().getId());
-    }
-
-    @Test
-    void getBookById_shouldReturnEmpty_whenNotFound() {
-        when(bookRepository.findById(1L)).thenReturn(Optional.empty());
-
-        Optional<Book> result = bookService.getBookById(1L);
-
-        assertTrue(result.isEmpty());
-    }
-}
+                java.util.Base64.getEncoder().enco
