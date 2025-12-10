542c7d5aa18ab7eb22b0a90b4def3a7476a84c1b
Liza <liza.ksenofontova1621@gmail.com>
2025-12-10 17:13:08 +0100

    Revert "Test V4.11"
    This reverts commit fefa4cb1fc64f26edb09ae32f7f5a09f03569de7.

--- src/main/java/bs/lf10/entity/User.java
+++ src/main/java/bs/lf10/entity/User.java
@@ -16,17 +16,4 @@ public class User {
     private String username;
     private String email;
     private String password;
-
-    /**
-     * Kopierkonstruktor, um einen Benutzer aus einem anderen Benutzerobjekt zu erstellen.
-     * Dieser behebt den Kompilierungsfehler in Book.java.
-     */
-    public User(User originalUser) {
-        if (originalUser != null) {
-            this.id = originalUser.id;
-            this.username = originalUser.username;
-            this.email = originalUser.email;
-            this.password = originalUser.password;
-        }
-    }
 }
