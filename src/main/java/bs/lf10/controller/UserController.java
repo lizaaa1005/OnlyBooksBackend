9016dcef3d206f74b169375ed89b8d0ec8517ebc
Liza <liza.ksenofontova1621@gmail.com>
2025-12-10 17:13:16 +0100

    Revert "Test V3.5"
    This reverts commit c3e9a6dfc926f5174c9ac083484f21feac97ef66.

--- src/main/java/bs/lf10/controller/UserController.java
+++ src/main/java/bs/lf10/controller/UserController.java
@@ -2,12 +2,14 @@ package bs.lf10.controller;
 
 import bs.lf10.entity.User;
 import bs.lf10.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+//@RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
@@ -21,22 +23,19 @@ public class UserController {
     public ResponseEntity<User> register(@RequestBody User user) {
         try {
             User newUser = userService.registerUser(user);
-            return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+            return new ResponseEntity<>(newUser, HttpStatus.CREATED);
         } catch (RuntimeException e) {
-            // Keine null-Body ResponseEntity mehr → SpotBugs zufrieden
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
         }
     }
 
     @PostMapping("/login")
     public ResponseEntity<User> login(@RequestBody User user) {
         try {
-            User authenticatedUser =
-                    userService.loginUser(user.getUsername(), user.getPassword());
-            return ResponseEntity.ok(authenticatedUser);
+            User authenticatedUser = userService.loginUser(user.getUsername(), user.getPassword());
+            return new ResponseEntity<>(authenticatedUser, HttpStatus.OK);
         } catch (RuntimeException e) {
-            // Auch hier: body() weggelassen → kein NP_NONNULL_PARAM_VIOLATION
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
         }
     }
 }
