c3e9a6dfc926f5174c9ac083484f21feac97ef66
littlelazymeerkat <esperanza.rugullis@gmail.com>
2025-12-10 14:22:39 +0100

    Test V3.5

--- src/main/java/bs/lf10/controller/UserController.java
+++ src/main/java/bs/lf10/controller/UserController.java
@@ -2,14 +2,12 @@ package bs.lf10.controller;
 
 import bs.lf10.entity.User;
 import bs.lf10.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-//@RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
@@ -23,19 +21,22 @@ public class UserController {
     public ResponseEntity<User> register(@RequestBody User user) {
         try {
             User newUser = userService.registerUser(user);
-            return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+            return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
         } catch (RuntimeException e) {
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            // Keine null-Body ResponseEntity mehr → SpotBugs zufrieden
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
     @PostMapping("/login")
     public ResponseEntity<User> login(@RequestBody User user) {
         try {
-            User authenticatedUser = userService.loginUser(user.getUsername(), user.getPassword());
-            return new ResponseEntity<>(authenticatedUser, HttpStatus.OK);
+            User authenticatedUser =
+                    userService.loginUser(user.getUsername(), user.getPassword());
+            return ResponseEntity.ok(authenticatedUser);
         } catch (RuntimeException e) {
-            return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
+            // Auch hier: body() weggelassen → kein NP_NONNULL_PARAM_VIOLATION
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
     }
 }
