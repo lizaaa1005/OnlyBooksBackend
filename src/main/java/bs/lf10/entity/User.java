37a294be772860257152bc9f69dd22da7a87e337
littlelazymeerkat <esperanza.rugullis@gmail.com>
2025-12-10 15:03:29 +0100

    Test V4.4

--- src/main/java/bs/lf10/entity/User.java
+++ src/main/java/bs/lf10/entity/User.java
@@ -1,42 +1,18 @@
-package bs.lf10.entity;
-
-import jakarta.persistence.*;
-import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
+@Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
 
-    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Book> books;
 
-    // ----------------- Konstruktor mit defensiven Kopien -----------------
-    public User(Long id, String username, String password, List<Book> books) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.books = books == null ? null : new ArrayList<>(books);
-    }
-
-    // ----------------- Getter / Setter -----------------
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
+    // Defensive copy für Bücher
     public List<Book> getBooks() {
         return books == null ? null : new ArrayList<>(books);
     }
