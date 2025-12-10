122bafb9baa9b55bc94bbc04ae72feddf11c37dc
littlelazymeerkat <esperanza.rugullis@gmail.com>
2025-12-10 15:59:16 +0100

    Test V4.12

--- src/test/java/bs/lf10/service/BookServiceTest.java
+++ src/test/java/bs/lf10/service/BookServiceTest.java
@@ -7,8 +7,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
 
@@ -26,7 +24,16 @@ public class BookServiceTest {
 
     @Test
     void saveBook_shouldNotFailWhenImagesAreNull() {
-        Book book = new Book(1L, "Test", "Author", true, "Description");
+        // KORRIGIERT: Verwenden des no-args Konstruktors und Settern
+        Book book = new Book();
+        book.setId(1L);
+        book.setTitle("Test");
+        book.setAuthor("Author");
+        book.setAvailable(true);
+        // "Description" wurde wahrscheinlich dem 'description' Feld zugewiesen, 
+        // da es nicht in den 5 Argumenten des alten Aufrufs enthalten war.
+        book.setDescription("Description"); 
+        
         book.setAdditionalImages(null);
 
         when(bookRepository.save(book)).thenReturn(book);
